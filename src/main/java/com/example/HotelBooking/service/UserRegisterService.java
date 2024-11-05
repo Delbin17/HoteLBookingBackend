25b8a9ae30a316ef56cb4f89a809c394b1da674f
Delbin <delbin2019@gmail.com>
2024-11-05 13:30:44 +0530

    qw

--- src/main/java/com/example/HotelBooking/service/UserRegisterService.java
+++ src/main/java/com/example/HotelBooking/service/UserRegisterService.java
@@ -1,4 +1,20 @@
 package com.example.HotelBooking.service;
 
+
+import com.example.HotelBooking.entity.UserData;
+import com.example.HotelBooking.repositry.UserRepositry;
+import org.aspectj.apache.bcel.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserRegisterService {
+
+    @Autowired
+    UserRepositry repo;
+
+    public void RegisterUser(UserData user){
+        repo.save(user);
+
+    }
 }

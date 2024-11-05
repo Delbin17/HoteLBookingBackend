176e58526a6d26082e10528208273b47d46315b6
Delbin <delbin2019@gmail.com>
2024-11-05 14:21:42 +0530

    we

--- src/main/java/com/example/HotelBooking/controller/UserRegisterController.java
+++ src/main/java/com/example/HotelBooking/controller/UserRegisterController.java
@@ -2,23 +2,38 @@ package com.example.HotelBooking.controller;
 
 
 import com.example.HotelBooking.entity.UserData;
+import com.example.HotelBooking.exception.HotelBookingException;
+import com.example.HotelBooking.model.DataModel;
 import com.example.HotelBooking.service.UserRegisterService;
+import com.example.HotelBooking.util.ResponseHandle;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("userRegister")
 public class UserRegisterController {
 
     @Autowired
     UserRegisterService service;
 
 
-    @PostMapping("register")
-    public void RegisterUser(UserData user){
-        service.RegisterUser(user);
+    @PostMapping("/register")
+    public ResponseEntity<?> RegisterUser(@RequestBody DataModel user){
+        try {
+
+
+            service.RegisterUser(user);
+            return ResponseHandle.registrationResponse(HttpStatus.OK, "success",null);
+
+        }catch(HotelBookingException h){
+            return ResponseHandle.registrationResponse(HttpStatus.BAD_REQUEST, "failed",h.getError());
+
+        }
 
 
     }
--- src/main/java/com/example/HotelBooking/service/UserRegisterService.java
+++ src/main/java/com/example/HotelBooking/service/UserRegisterService.java
@@ -2,10 +2,14 @@ package com.example.HotelBooking.service;
 
 
 import com.example.HotelBooking.entity.UserData;
+import com.example.HotelBooking.exception.HotelBookingException;
+import com.example.HotelBooking.model.DataModel;
 import com.example.HotelBooking.repositry.UserRepositry;
 import org.aspectj.apache.bcel.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserRegisterService {
@@ -13,8 +17,19 @@ public class UserRegisterService {
     @Autowired
     UserRepositry repo;
 
-    public void RegisterUser(UserData user){
-        repo.save(user);
+    public void RegisterUser(DataModel user){
+
+        UserData userData=new UserData();
+
+        userData.setAddress(user.getAddress());
+        userData.setEmail(user.getEmail());
+        userData.setPassword(user.getPassword());
+        userData.setAadharNumber(user.getAadharNumber());
+        userData.setFirstName(user.getFirstName());
+        userData.setLastName(user.getLastName());
+        userData.setPhone(user.getMobile());
+
+        repo.save(userData);
 
     }
 

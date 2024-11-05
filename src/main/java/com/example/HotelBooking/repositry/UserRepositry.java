ed9145b16964c2b46b421b14cc43fe3ec710325d
shajinshan <shajinshajin02@gmail.com>
2024-11-05 13:48:30 +0530

    modelup

--- src/main/java/com/example/HotelBooking/repositry/UserRepositry.java
+++ src/main/java/com/example/HotelBooking/repositry/UserRepositry.java
@@ -1,4 +1,13 @@
 package com.example.HotelBooking.repositry;
 
-public interface UserRepositry {
+import com.example.HotelBooking.entity.UserData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepositry extends JpaRepository<UserData,Long> {
+
+    @Query("SELECT c FROM UserData c WHERE c.email = :email AND c.password = :password ")
+    UserData checkValidLogin(@Param("email") String gmail, @Param("password") String password);
+
 }

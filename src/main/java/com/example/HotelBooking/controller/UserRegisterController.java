690d7155f9c67de4493a3737eebf1d30d0183b4c
Delbin <delbin2019@gmail.com>
2024-11-05 13:47:29 +0530

    Save local changes before pulling

--- src/main/java/com/example/HotelBooking/controller/UserRegisterController.java
+++ src/main/java/com/example/HotelBooking/controller/UserRegisterController.java
@@ -4,6 +4,7 @@ package com.example.HotelBooking.controller;
 import com.example.HotelBooking.entity.UserData;
 import com.example.HotelBooking.service.UserRegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,8 @@ public class UserRegisterController {
     @Autowired
     UserRegisterService service;
 
+
+    @PostMapping("register")
     public void RegisterUser(UserData user){
         service.RegisterUser(user);
 

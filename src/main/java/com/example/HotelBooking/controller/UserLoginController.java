e28b15d4f0cee2d3233725ea3afc41bc680d935c
shajinshan <shajinshajin02@gmail.com>
2024-11-05 15:30:43 +0530

    Your commit message

--- src/main/java/com/example/HotelBooking/controller/UserLoginController.java
+++ src/main/java/com/example/HotelBooking/controller/UserLoginController.java
@@ -8,16 +8,14 @@ import com.example.HotelBooking.util.ResponseHandle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/userLogin")
+
 public class UserLoginController {
 
     @Autowired
